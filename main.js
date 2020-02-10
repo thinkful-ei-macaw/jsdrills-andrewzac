2da2adc8b41e0c99a5ce7e5deb1acaf789014d26
Andrew Durrant <andrew.durrant75@gmail.com>
2020-02-10 16:49:42 -0700

    refactor main.js drill 5

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 /* eslint-disable strict */
 const createGreeting = (name, age) => {
-  return `Hi, my name is ${name} and I'm ${age} years old`;
+  const yearOfBirth = 2020 - age;
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 };
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('andy', 35);
 // eslint-disable-next-line no-console
 console.log(greeting1);
