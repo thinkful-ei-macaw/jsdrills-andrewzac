b7ae1f9b1bdb92b24994ee40fbcca8a2f9a9c4fb
Andrew Durrant <andrew.durrant75@gmail.com>
2020-02-10 18:59:29 -0700

    refactor main.js drill 7

--- main.js
+++ main.js
@@ -3,8 +3,21 @@ const createGreeting = (name, age) => {
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 };
 
-const getYearOfBirth = age => 2020 - age;
+const getYearOfBirth = age => {
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  }
+  return 2020 - age;
+};
 
-const greeting1 = createGreeting('andy', 35);
+
+try {
+  const greeting1 = createGreeting('andy', 35);
+  console.log(greeting1);
+  
+} catch(error) {
+  console.log(error);
+}
+
 // eslint-disable-next-line no-console
-console.log(greeting1);
+
