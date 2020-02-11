30a6ef7e802529ebdf1c55c50e97bf4e931735d9
Andrew Durrant <andrew.durrant75@gmail.com>
2020-02-10 19:21:01 -0700

    refactor main.js drill 8

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 /* eslint-disable strict */
 const createGreeting = (name, age) => {
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  } else if (typeof age !== 'number' || typeof name !== 'string') {
+    throw new TypeError('Invalid input type');
+  }
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 };
 
@@ -12,11 +17,11 @@ const getYearOfBirth = age => {
 
 
 try {
-  const greeting1 = createGreeting('andy', 35);
+  const greeting1 = createGreeting('Bob', 77);
   console.log(greeting1);
   
 } catch(error) {
-  console.log(error);
+  console.log(`${error.name}: ${error.message}`);
 }
 
 // eslint-disable-next-line no-console
