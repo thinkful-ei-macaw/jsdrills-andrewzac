d729ba9c650e71ca0cafd0a4396149ea97d1cde8
Zac Barreca <zacbarreca@gmail.com>
2020-02-11 08:55:29 -0500

    js drill 1 step 7 and step 7

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-/* eslint-disable strict */
-const createGreeting = (name, age) => {
-  let yearOfBirth = 2020 - age;
-  return `Hi, my name is ${name} and I'm ${age} years old I was born in ${yearOfBirth}`;
-  
+'use strict';
+
+const getYearOfBirth = age => {
+  if (age < 0) {
+    throw new Error('Age can not be negative.');
+  }
+  return 2020 - age;
 };
 
-const greeting1 = createGreeting("Zac", 24);
-// eslint-disable-next-line no-console
-console.log(greeting1);
+const createGreeting = (name, age) => {
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  } else if (typeof age !== 'number' || typeof name !== 'string') {
+    throw new TypeError('Invalid input type');
+  }
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
+};
 
+try {
+  const greeting1 = createGreeting('Zac', 24);
+  console.log(greeting1);
+} catch(error) {
+  console.log('There is some error');
+}
